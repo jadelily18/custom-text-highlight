d70fa6e91a638b4966cc492d4623b19eeb6aee27
Jade Nash <jade.nashh18@gmail.com>
2022-08-09 05:35:17 -0500

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import './app.css';
+
+	import Selector from './lib/components/selector.svelte';
+</script>
+
+<!-- TODO: add actual save and load functionality (svelte stores?) with a toggle checkbox -->
+
+<div class="flex flex-col w-72 p-6 dark:bg-gray-800">
+	<h1 class="pb-6 text-xl text-center text-gray-900 dark:text-white font-bold">
+		Custom Highlight Color
+	</h1>
+	<div class="selectors space-y-3">
+		<Selector label="Background Highlight" placeholder="Background Highlight" />
+		<Selector label="Text Highlight" placeholder="Text Highlight" />
+	</div>
+	<button
+		class="save-button w-full p-2.5 mt-8 transition-colors bg-blue-500 hover:bg-blue-600 active:bg-blue-700 text-white font-semibold rounded-md"
+		>Save</button
+	>
+</div>
+
+<div class="flex p-2 pt-0.5 px-4 justify-between font-semibold dark:bg-gray-800 dark:text-white">
+	<p>v1.0.0</p>
+	<a
+		class="hover:text-gray-500 transition-colors"
+		href="https://github.com"
+		target="_blank"
+		rel="noreferrer noopener">Source</a
+	>
+</div>
+
+<style lang="scss">
+	// .popup {
+	// 	// background-color: #242424;
+	// }
+</style>
---
+++ src/lib/components/selector.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	export let label: string;
+	export let placeholder: string;
+	export let color: string = '#942929';
+
+	function changeColor(newColor: string) {
+		color = newColor;
+	}
+</script>
+
+<h1 class="text-md font-semibold text-gray-700 dark:text-gray-50">{label}</h1>
+<div
+	class="selector flex w-full h-10 group rounded-md outline outline-1 outline-offset-0 outline-gray-300 dark:outline-gray-900 hover:outline-gray-700 dark:hover:outline-black transition-all overflow-clip"
+>
+	<input
+		class="peer w-4/6 py-2 px-3.5 outline-none bg-gray-100 group-hover:bg-gray-100 placeholder:text-gray-500 focus:placeholder:text-gray-800 group-hover:placeholder:text-gray-800 placeholder:font-semibold placeholder:italic transition-all"
+		type="text"
+		bind:value={color}
+		{placeholder}
+	/>
+	<div class="color-wrapper grow h-full p-0 m-0" style="background-color: {color};">
+		<input class="selector-color h-full" type="color" bind:value={color} />
+	</div>
+</div>
+
+<style lang="scss">
+	.selector {
+		:hover {
+			outline: 3px;
+		}
+	}
+	.selector-color {
+		opacity: 0;
+	}
+	::selection {
+		color: var(--color);
+		background: var(--color);
+	}
+</style>
